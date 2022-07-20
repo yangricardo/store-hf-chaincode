9ecf1afa5c1432022c17daba01c599690ae5b306
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2022-07-20 17:45:45 -0300

    perf: improvements of chaincode object error

--- src/helpers/chaincode.error.ts
+++ src/helpers/chaincode.error.ts
@@ -1,4 +1,5 @@
 export class ChaincodeError<RawErrorType = any> {
+	name: "ChaincodeError";
 	message: string;
 	status: string | number;
 	details?: RawErrorType;
@@ -10,5 +11,18 @@ export class ChaincodeError<RawErrorType = any> {
 		this.message = message;
 		this.status = status;
 		this.details = details;
+		console.error(this);
+	}
+
+	static fromError(error: ChaincodeError | Error | unknown) {
+		if (error instanceof ChaincodeError) {
+			return error;
+		} else if (error instanceof Error) {
+			return new ChaincodeError(error.message, 500, {
+				name: error.name,
+				stack: error.stack,
+			});
+		}
+		return new ChaincodeError("Unkown Error", 500, error);
 	}
 }
